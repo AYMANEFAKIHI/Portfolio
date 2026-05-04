b55bf706d8e586ba66bc2cec60b7fa3c04f6dd71
AYMANEFAKIHI <aymanefakihi@users.noreply.github.com>
2026-05-04 11:16:03 +0100

    gg

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['via.placeholder.com'],
+  },
+  webpack(config) {
+    config.resolve.alias['@splinetool/react-spline'] = path.resolve(
+      __dirname,
+      'node_modules/@splinetool/react-spline/dist/react-spline.js'
+    )
+    return config
   },
 }
 
